b5b45a74e313762cff69271df9088e6ac8e363f0
PolyMagic <bartek.marynczak@op.pl>
2018-04-22 13:07:29 +0200

    init

---
+++ src/pac/Note.java
@@ -0,0 +1,71 @@
+package pac;
+
+import processing.core.*;
+
+ public class Note{
+  Start parent;
+  
+  PImage DisplayImage;
+  Boolean black = false;
+  
+  int Color = 91;
+  
+  float x =0;
+  float y = -10;
+  
+  float h = 3;
+  
+  float offset = 0;
+  
+  Key key;
+  
+  Boolean NoLongerActive = false;
+  
+  long last_time = System.nanoTime();
+  
+  Note(Start p,Key key,int Color){
+    parent = p;
+    this.key = key;
+    this.x = key.x;
+    this.black = key.Black;
+    this.Color = Color;
+  }
+  
+  void draw(){
+    
+    if(!this.black) {
+    	parent.fill(this.Color,300,300);
+    	parent.rect(this.x,this.y,36,this.h,7);
+    }
+    else {
+    	parent.fill(this.Color,300,300);
+    	parent.rect(this.x-15,this.y,24,this.h,7);
+    }
+    
+    
+  }
+  
+  void update(int delta_time){
+    
+  
+	this.y-= 1*delta_time;
+
+	if(this.y < -parent.height-this.h) {
+		parent.Notes.remove(this);
+	}
+	
+	if(!NoLongerActive) {
+		
+		 if(this.key.Active) {
+		    	this.h+= 1*delta_time;
+		    }
+		 else NoLongerActive=true;
+		
+	}
+   
+	  
+	  
+  }
+  
+  
+}
---
+++ src/pac/Player.java
@@ -0,0 +1,108 @@
+package pac;
+
+/* http://blog.taragana.com/index.php/archive/how-to-play-a-midi-file-from-a-java-application/ */
+import javax.sound.midi.*;
+import java.io.*;
+
+//import java.util.ArrayList;
+
+public class Player extends Thread {
+String File = "44.mid";
+Start RootParrent;
+
+public class myListener implements Receiver {
+public void send(MidiMessage message, long timeStamp)
+{
+    if (message instanceof ShortMessage)
+    {
+        ShortMessage sm      = (ShortMessage) message;
+        int          channel = sm.getChannel();
+        int          command = sm.getCommand();
+
+
+
+
+        if (command == ShortMessage.NOTE_ON)
+        {
+//               String[] NOTE_NAMES = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
+//               int note = sm.getData1() % 12;
+//               String name = NOTE_NAMES[note];
+
+//                System.out.println( channel );
+
+            int     rawKey = sm.getData1() - 12 * 2;
+
+            Boolean State = true;
+
+            if (sm.getData2() == 0)
+                State = false;
+            else
+                State = true;
+
+            RootParrent.noteTrigger(rawKey, channel, State);
+        }
+        else if (command == ShortMessage.NOTE_OFF)
+        {
+            int rawKey = sm.getData1() - 12 * 2;
+
+            RootParrent.noteTrigger(rawKey, channel, false);
+        }
+    }
+}
+public void close()
+{
+    System.out.println("O");
+}
+}
+
+
+Player(String file, Start Parrent)
+{
+    this.RootParrent = Parrent;
+}
+
+public void run()
+{
+    File midiFile = new File(this.File);
+
+    // Play once
+    try {
+        Sequencer sequencer = MidiSystem.getSequencer();
+        sequencer.setSequence(MidiSystem.getSequence(midiFile));
+        sequencer.open();
+
+
+        Receiver    receiver = new myListener();
+
+        Transmitter transmitter = sequencer.getTransmitter();
+
+        transmitter.setReceiver(receiver);
+
+        sequencer.start();
+
+
+
+
+        while (true)
+        {
+            if (sequencer.isRunning())
+            {
+                try {
+                    Thread.sleep(1000);     // Check every second
+                } catch (InterruptedException ignore) { break; }
+            }
+            else
+                break;
+        }
+        // Close the MidiDevice & free resources
+        sequencer.stop();
+        sequencer.close();
+    } catch (MidiUnavailableException mue) {
+        System.out.println("Midi device unavailable!");
+    } catch (InvalidMidiDataException imde) {
+        System.out.println("Invalid Midi data!");
+    } catch (IOException ioe) {
+        System.out.println("I/O Error!");
+    }
+}
+}
